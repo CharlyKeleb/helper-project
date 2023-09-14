e1d9b7bd45cc4992bd1c0fa72be5dc2be2a7429b
GabbyGreat <gabrieloranekwu@gmail.com>
2023-09-14 16:13:28 +0100

    :sparkles: Smart design

--- tests/lib/provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:tests/network.dart';
-
-var jokeProvider =
-    FutureProvider.family((ref, String type) => NetworkRequest.getJoke(type));
